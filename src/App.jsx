cbe2dbbe6ed2ebe4652ff5a1f92304d35df7f3b7
John Michael <johnny25580@gmail.com>
2023-06-26 12:33:29 +0100

    header , navbar pages added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './components/Header/Header'
 import { BrowserRouter as Router,Route ,Routes} from 'react-router-dom'
 import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
        <Routes>
         <Route path='/' element={<Header/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/contact' element={<  Contact/>}/>
+        <Route path='/footer' element={<Footer/>}/>
 
        </Routes>
     </Router>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom"
 import styles from "./Header.module.scss"
+import {FaShoppingCart} from  'react-icons/fa'
+import {HiOutlineMenuAlt3} from 'react-icons/hi'
 
 function Header() {
-  const logo = 
-  <Link to="/home"><h2>e <span>Jumia</span>.</h2></Link>
+  const logo = (
+  <Link to="/home"><h2>e <span>BestBuy</span>.</h2></Link>)
 
-
+  const cart =(
+      <span className={styles.cart}>
+                 <Link to="/cart">
+                    Cart
+                   <FaShoppingCart />
+                   <p>0</p>
+                  </Link>
+                 </span>
+  )
   return (
     <header>
          <div className={styles.header}> 
@@ -15,17 +25,44 @@ function Header() {
 
            <nav>
              <ul>
+                
 
                 <li>
                   <Link to="/home">Home</Link>
                 </li>
 
                 <li>
-                  <Link to="/home">Contact</Link>
+                  <Link to="/contact">Contact</Link>
                 </li>
+           
 
              </ul>
            </nav>
+
+           <section >
+               <ul>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              
+               
+                <li>
+                  <Link to="/login">Register</Link>
+                </li>
+
+               <li>
+                  <Link to="/order-history">My order
+               </Link>
+                </li>
+             </ul>
+                {cart}
+                 
+           </section>
+
+           <div className={styles["menu-icon"]}>
+              {cart}
+              <HiOutlineMenuAlt3/>
+           </div>
 
                    
          </div>
